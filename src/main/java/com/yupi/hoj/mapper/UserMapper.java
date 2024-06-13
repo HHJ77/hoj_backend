b232543c7b3eb371e5b710c910c4fd88b5ebd241
Hyj <2856042536@qq.com>
2024-06-13 15:50:42 +0800

    项目模板初始化

---
+++ src/main/java/com/yupi/hoj/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.yupi.hoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.hoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
